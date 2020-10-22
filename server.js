bd24c40893bfe961e10047d507dd93709436124c
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-10-22 18:56:15 -0500

    Added posts and added delete dont know if it works

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-// Dependencies
-// =============================================================
+// Dependancies TODO: Spell Check ME
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
@@ -22,6 +21,7 @@ let notes = [];
 
 //Routes
 
+//Gets
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
@@ -36,6 +36,42 @@ app.get("/api/notes", function (err, res) {
     notes = JSON.parse(notes);
     res.json(notes);
 });
+
+//Posts
+app.post("/api/notes", function (req, res) {
+    notes = fs.readFileSync("db/db.json", "utf8");
+    console.log(notes);
+    notes = JSON.parse(notes);
+    req.body.id = notes.length;
+    notes.push(req.body);
+    notes = JSON.stringify(notes);
+    fs.writeFile("db/db.json", notes, "utf8", function (err) {
+        if (err) throw err;
+    });
+    res.json(JSON.parse(notes));
+});
+
+//Delete A Note
+app.delete("/api/notes/:id", function (req, res) {
+
+    notes = fs.readFileSync("db/db.json", "utf8");
+
+    notes = JSON.parse(notes);
+
+    notes = notes.filter(function (note) {
+        return note.id != req.params.id;
+    });
+
+    fs.writeFile("db/db.json", notes, "utf8", function (err) {
+
+        if (err) throw err;
+    });
+    notes = JSON.stringify(notes);
+
+
+    res.send(JSON.parse(notes));
+});
+
 
 // Starts the server to begin listening
 // =============================================================
