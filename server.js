05113bc4873ff9b9ac5c3da186262c2e9b2b7aa1
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-10-22 21:48:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ let notes = [];
 //================================
 //Gets
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 //TODO: Comment this
